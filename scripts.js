503d53dace8dcbfac3593ad7ba2bfc9ed0d350e0
ayyu <ayyu@umich.edu>
2018-12-26 20:34:22 -0500

    now includes time but it does not update automatically

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 var request = new XMLHttpRequest();
 
 request.open('GET',
-'https://api.unsplash.com/photos/random/auto=format/?client_id=<UR KEY HERE>', true);
+'https://api.unsplash.com/photos/random/?client_id=&query=nature,water,flowers,plants,lights,city', true);
+
 request.onload = function () {
 
   // Begin accessing JSON data here
@@ -10,6 +11,34 @@ request.onload = function () {
   var image_link = data["urls"]["regular"];
   var complete_link = "url(" + image_link + "}";
   document.body.style.backgroundImage = complete_link;
+  document.body.style.backgroundSize = "1290px 620px";
+}
+//request.send();
+document.body.style.backgroundColor = '#E0AC9D';
+
+var day = new Date();
+var weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var greeting = document.getElementById('greeting');
+greeting.innerText = "Happy " + weekday[day.getDay()] + ", Angela."
+
+var time = document.getElementById('time');
+var hour = day.getHours() % 12;
+if (hour == 0){
+  hour = 12;
 }
 
-request.send();
+var min = day.getMinutes();
+
+if (min < 10){
+  min = "0" + min;
+}
+
+time.innerText = hour + ":" + min;
